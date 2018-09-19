b1dde00043447404aa0221c0b008db8797486c62
Amarjot Parmar <amarjotparmar098@gmail.com>
2018-09-20 11:01:54 +1200

    add activities/java files (recorded msgs, send msg)

---
+++ app/src/main/java/com/parmar/amarjot/android_sqlite_example/RecordedMessagesActivity.java
@@ -0,0 +1,6 @@
+package com.parmar.amarjot.android_sqlite_example;
+
+public class RecordedMessagesActivity {
+
+    // Read SQL db and display messages
+}
---
+++ app/src/main/java/com/parmar/amarjot/android_sqlite_example/SendMessageActivtiy.java
@@ -0,0 +1,7 @@
+package com.parmar.amarjot.android_sqlite_example;
+
+public class SendMessageActivtiy {
+
+    // When send button pressed take users input and store in database.
+    // Clear Textbox and make a toast.
+}
